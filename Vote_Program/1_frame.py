1a2c03634cec3be23edcbe9f051eb6e2fde68130
DESKTOP-QJU6CR4\pinter <pgpss123@gmail.com>
2023-04-09 14:13:20 +0900

    123

--- Vote_Program/1_frame.py
+++ Vote_Program/1_frame.py
@@ -12,10 +12,12 @@ ID_label = Label(root, text="ID", width=5, height=2)
 ID_entry = Entry(root, width=20)
 PW_label = Label(root, text="PW", width=5, height=2)
 PW_entry = Entry(root, width=20)
+Login_button = Button(root, text="로그인", width=5, height=2)
 
 ID_label.grid(row=0, column=0, padx=3, pady=3)
 ID_entry.grid(row=0, column=1, padx=3, pady=3)
 PW_label.grid(row=1, column=0, padx=3, pady=3)
 PW_entry.grid(row=1, column=1, padx=3, pady=3)
+Login_button.grid(row=2, column=0, columnspan=2, padx=3, pady=3)
 
 root.mainloop()
